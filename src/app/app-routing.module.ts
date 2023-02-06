0e3da0cf8bb06fa60a1bfd5749e93bcc649fc3e2
Nitro-Calculus <ggiorgxviii@outlook.fr>
2023-02-06 09:12:37 -0300

    hero edit (wip)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { HeroEditComponent } from './hero-edit/hero-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeroesListComponent
+  },
+  {
+    path: 'edit/:id',
+    component: HeroEditComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+
